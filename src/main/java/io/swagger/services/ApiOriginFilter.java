03eb54da7a098bb6a535c0c7e163c149bbb2dff2
JulianWeghaupt <if21b100@technikum-wien.at>
2022-09-24 17:49:57 +0200

    cleaned up generator mess

--- src/main/java/io/swagger/services/ApiOriginFilter.java
+++ src/main/java/io/swagger/services/ApiOriginFilter.java
@@ -1,4 +1,4 @@
-package io.swagger.api;
+package io.swagger.services;
 
 import java.io.IOException;
 
--- src/main/java/io/swagger/services/NotFoundException.java
+++ src/main/java/io/swagger/services/NotFoundException.java
@@ -1,10 +1,10 @@
-package io.swagger.api;
+package io.swagger.services;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-09-24T14:25:41.651Z[GMT]")
-public class ApiException extends Exception {
+public class NotFoundException extends ApiException {
     private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
         this.code = code;
     }
 }
